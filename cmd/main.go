db5e4f32484e898db45ba19f09ce6121fd55203c
fuadnafiz98 <fuadnafiz98@gmail.com>
2025-08-14 00:43:27 +0600

    feat: simple select

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,27 @@
 package main
 
 import (
+	"fmt"
+
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
 type model struct {
-	text string
+	choices  []string
+	cursor   int
+	selected map[int]bool
+}
+
+func initialModel() model {
+	return model{
+		choices:  []string{"docker image 1", "docker image 2", "docker image 3"},
+		selected: make(map[int]bool),
+	}
 }
 
 func (m model) Init() tea.Cmd {
 	return nil
-}
-
-func (m model) View() string {
-	style := lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("5"))
-	return style.Render(m.text) + "\n\nPress q to Quit."
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -24,14 +30,65 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 		case "q", "ctrl+c":
 			return m, tea.Quit
+
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+
+		case "enter", " ":
+			if m.selected[m.cursor] {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = true
+			}
 		}
 	}
 	return m, nil
 }
 
+func (m model) View() string {
+	titleStyle := lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("#ffffff"))
+	itemStyle := lipgloss.NewStyle()
+	selectedItemStyle := lipgloss.NewStyle()
+	cursorStyle := lipgloss.NewStyle()
+
+	s := titleStyle.Render("Choose a Docker image:")
+	s += "\n"
+
+	for i, choice := range m.choices {
+		cursor := ""
+		if m.cursor == i {
+			cursor = cursorStyle.Render(">")
+		}
+
+		checked := " "
+		if m.selected[i] {
+			checked = "x"
+		}
+
+		line := fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+
+		if m.cursor == i {
+			s += selectedItemStyle.Render(line)
+		} else {
+			s += itemStyle.Render(line)
+		}
+	}
+
+	footerStyle := lipgloss.NewStyle().Foreground(lipgloss.Color("#f0f0f0")).MarginTop(1)
+	footer := footerStyle.Render("\nPress q to quit.")
+	s += "\n" + footer
+	return s
+}
+
 func main() {
-	initialModel := model{text: "Hello, World!"}
-	p := tea.NewProgram(initialModel, tea.WithAltScreen())
+	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		panic(err)
 	}
